905525a00522aa03c49215ffe68b775551bbec43
Matheus Dresch <--local>
2024-01-10 17:03:44 -0300

    Adicionando icones

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,10 @@
+import gh from "./gh.svg";
+import insta from "./insta.svg";
+import js from "./js.svg";
+import laravel from "./laravel.svg";
+import lin from "./lin.svg";
+import mysql from "./mysql.svg";
+import php from "./php.svg";
+import vue from "./vue.svg";
+
+export { gh, insta, js, laravel, lin, mysql, php, vue };
