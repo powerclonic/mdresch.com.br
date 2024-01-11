b07578fbab848372901422876e8d3f10f0ccb04d
Matheus Dresch <matheus.dresch02@gmail.com>
2024-01-11 16:56:23 -0300

    Desktop finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <v-app>
     <v-main>
-      <TheHeader />
-      <article class="content">
-        <TheResume />
-        <TheStack />
-        <TheLanguages />
-        <TheProjects />
-        <TheMessage />
-        <TheSocials />
-      </article>
-      <TheFooter />
+      <div class="container">
+        <TheHeader />
+        <article class="content">
+          <TheResume />
+          <TheStack />
+          <TheLanguages />
+          <TheProjects class="div1" />
+          <TheMessage class="div2" />
+          <TheSocials />
+        </article>
+        <TheFooter />
+      </div>
     </v-main>
   </v-app>
 </template>
@@ -24,10 +26,37 @@ body {
   font-family: "Fredoka", Arial, Helvetica, sans-serif;
 }
 
+.container {
+  display: flex;
+  flex-direction: column;
+
+  justify-content: space-between;
+
+  height: 100%;
+}
 .content {
   padding: 10px;
   display: flex;
   flex-direction: column;
   gap: 20px;
+}
+
+@media screen and (min-width: 769px) {
+  .content {
+    padding: 32px 12.5vw;
+
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+  }
+
+  .div1 {
+    grid-area: 2 / 2 / 4 / 3;
+  }
+  .div2 {
+    grid-area: 3 / 1 / 5 / 2;
+  }
 }
 </style>
--- src/components/TheMessage.vue
+++ src/components/TheMessage.vue
@@ -4,18 +4,20 @@
     <!-- <p>gostou? entre em contato comigo</p>
     <v-container>
       <v-row>
-        <v-col class="d-flex flex-column flex-sm-row justify-space-between">
+        <v-col class="d-flex flex-column flex-sm-row justify-space-between p-0">
           <v-text-field
             class="mx-2"
             variant="solo"
             label="Seu nome"
             density="compact"
+            :hide-details="true"
           />
           <v-text-field
             class="mx-2"
             variant="solo"
             label="Seu e-mail"
             density="compact"
+            :hide-details="true"
           />
         </v-col>
       </v-row>
@@ -27,13 +29,15 @@
               variant="solo"
               label="Assunto"
               density="compact"
+              :hide-details="true"
             />
           </div>
-          <v-textarea
+          <v-text-field
             class="mx-2"
             variant="solo"
             label="Sua mensagem"
             density="compact"
+            :hide-details="true"
           />
         </v-col>
       </v-row>
